2d5e5957d4d31060583a41cc92866528c5404ed3
pursuit-monique <moniquecorrea@pursuit.orgname>
2023-04-23 08:53:32 -0400

    some changes

--- src/components/MoreInfo.jsx
+++ src/components/MoreInfo.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function MoreInfo() {
-  return <div className="more-info">MoreInfo</div>;
+  return (
+    <div class="alert alert-success">
+    <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+    <strong>Well done!</strong> You successfully read <a href="#" class="alert-link">this important alert message</a>.
+  </div>)
 }
 
 export default MoreInfo;
