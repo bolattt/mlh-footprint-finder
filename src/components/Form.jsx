fafb13f838de04ccfffbd6600f054e3126ef0dbc
Bo Latt <bolattny@gmail.com>
2023-04-23 00:05:01 -0400

    add form contents

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,28 +1,66 @@
+import { useState } from "react";
 import axios from "axios";
 
-const options = {
-  method: "GET",
-  url: "https://carbonfootprint1.p.rapidapi.com/CleanHydroToCarbonFootprint",
-  params: { energy: "Solar", consumption: "500" },
-  headers: {
-    "X-RapidAPI-Key": process.env.REACT_APP_API_KEY,
-    "X-RapidAPI-Host": "carbonfootprint1.p.rapidapi.com",
-  },
-};
-
 function Form() {
+  const [kwh, setKwh] = useState("");
+  const [source, setSource] = useState("solar");
+
   function handleSubmit(e) {
     e.preventDefault();
-    axios
-      .request(options)
-      .then(function (response) {
-        console.log(response.data);
-      })
-      .catch(function (error) {
-        console.error(error);
-      });
+    const options = {
+      method: "GET",
+      url: "https://carbonfootprint1.p.rapidapi.com/CleanHydroToCarbonFootprint",
+      params: { energy: source, consumption: kwh },
+      headers: {
+        "X-RapidAPI-Key": process.env.REACT_APP_API_KEY,
+        "X-RapidAPI-Host": "carbonfootprint1.p.rapidapi.com",
+      },
+    };
+    console.log(options);
+    // axios
+    //   .request(options)
+    //   .then(function (response) {
+    //     console.log(response.data);
+    //   })
+    //   .catch(function (error) {
+    //     console.error(error);
+    //   });
   }
-  return <div className="form">Form</div>;
+  console.log(kwh);
+  console.log(source);
+  return (
+    <div className="form p-3">
+      <form onSubmit={handleSubmit}>
+        <div className="form-group mb-2">
+          <label htmlFor="source">Example select</label>
+          <select
+            className="form-control"
+            id="source"
+            onChange={(e) => setSource(e.target.value)}
+          >
+            <option>Solar</option>
+            <option>Wind</option>
+            <option>Hydro Electric</option>
+          </select>
+        </div>
+
+        <div className="form-group mb-2">
+          <label htmlFor="kwh">The amount of energy consumed in KWH</label>
+          <input
+            type="number"
+            className="form-control"
+            id="kwh"
+            placeholder="kwh"
+            onChange={(e) => setKwh(e.target.value)}
+          />
+        </div>
+
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default Form;
--- src/components/FormContainer.jsx
+++ src/components/FormContainer.jsx
@@ -2,12 +2,10 @@ import Form from "./Form";
 
 function FormContainer() {
   return (
-    <div className="form-container">
-      FormContainer
+    <div className="form-container p-3">
       <img
         src="https://cdn.cundall.com/uploads/images/Blog/_large_image/101591/Reducing-our-personal-carbon-footprint.webp?v=1669306425"
         alt="img"
-        width={400}
       />
       <Form />
     </div>
