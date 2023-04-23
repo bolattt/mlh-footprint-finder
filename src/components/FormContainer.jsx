a289f282c8736326ea6262ea25f554a61dfdb768
pursuit-monique <moniquecorrea@pursuit.orgname>
2023-04-23 08:28:19 -0400

    here

--- src/components/FormContainer.jsx
+++ src/components/FormContainer.jsx
@@ -7,17 +7,25 @@ function FormContainer() {
     "https://slack-imgs.com/?c=1&o1=ro&url=https%3A%2F%2Fassets.justenergy.com%2Fwp-content%2Fuploads%2F2013%2F04%2FiStock_000014351447Medium-1.jpg"
   );
   return (
-    <div className="form-container p-5">
-      <img src={imgLink} alt="img" width={400} />
-      <Form
+    <div className="card mb-3" style={{maxWidth: "540px"}}>
+  <div className="row g-0">
+    <div class="col-md-4">
+    <img src={imgLink} className="img-fluid rounded-start" alt="..." />
+    </div>
+    <div className="col-md-8">
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <Form
         footprint={footprint}
         setFootprint={setFootprint}
         setImgLink={setImgLink}
       />
-
+        <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
   </div>
+</div>
 
-    
   );
 }
 
