70c92597219401dd53a59280e1ada259a711a804
pursuit-monique <moniquecorrea@pursuit.orgname>
2023-04-23 06:49:53 -0400

    Card placeholder

--- src/components/FormContainer.jsx
+++ src/components/FormContainer.jsx
@@ -2,15 +2,23 @@ import Form from "./Form";
 
 function FormContainer() {
   return (
-    <div className="form-container">
-      FormContainer
-      <img
-        src="https://cdn.cundall.com/uploads/images/Blog/_large_image/101591/Reducing-our-personal-carbon-footprint.webp?v=1669306425"
-        alt="img"
-        width={400}
-      />
-      <Form />
+    <div className="card text-bg-primary mb-3" style={{maxWidth: "540px"}}>
+  <div className="row g-0">
+    <div className="col-md-4">
+      <img src="..." class="img-fluid rounded-start" alt="..." />
     </div>
+    <div className="col-md-8">
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p className="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+  </div>
+  <Form />
+</div>
+
+    
   );
 }
 
