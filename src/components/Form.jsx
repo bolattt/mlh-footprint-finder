b6bc65d230f0096cf12c53037eb181ca5ad881b9
Bo Latt <bolattny@gmail.com>
2023-04-23 08:13:51 -0400

    add formula

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,17 +1,31 @@
 import { useState } from "react";
-import axios from "axios";
 
-function Form() {
+function Form({ setFootprint, footprint, setImgLink }) {
   const [electric, setElectric] = useState("");
   const [gas, setGas] = useState("");
   const [yearlyMileage, setYearlyMileage] = useState("");
   const [flights, setFlights] = useState(0);
   const [flightsOver4hr, setFlightsOver4hr] = useState(false);
-  const [recyclePaper, setRecyclePaper] = useState(false);
-  const [recycleTin, setRecycleTin] = useState(false);
 
   function handleSubmit(e) {
     e.preventDefault();
+
+    let total =
+      electric * 105 +
+      gas * 105 +
+      yearlyMileage * 0.79 +
+      flights * 1100 +
+      flightsOver4hr * 4400;
+    if (total < 16000) {
+      setImgLink("/low.png");
+      setFootprint("low");
+    } else if (total < 22000) {
+      setImgLink("/average.png");
+      setFootprint("average");
+    } else {
+      setImgLink("/high.png");
+      setFootprint("high");
+    }
   }
 
   return (
@@ -20,10 +34,64 @@ function Form() {
         <div className="form-group mb-2">
           <label htmlFor="electric">Monthly Electric Bill</label>
           <input
-            type="text"
+            type="number"
             className="form-control"
             id="electric"
             placeholder="Electric Bill"
+            required
+            onChange={(e) => setElectric(e.target.value)}
+          />
+        </div>
+
+        <div className="form-group mb-2">
+          <label htmlFor="gas">Monthly Gas Bill</label>
+          <input
+            type="number"
+            className="form-control"
+            id="gas"
+            placeholder="Gas Bill"
+            required
+            onChange={(e) => setGas(e.target.value)}
+          />
+        </div>
+
+        <div className="form-group mb-2">
+          <label htmlFor="mileage">Yearly Car Mileage</label>
+          <input
+            type="number"
+            className="form-control"
+            id="gas"
+            placeholder="Car Mileage"
+            required
+            onChange={(e) => setYearlyMileage(e.target.value)}
+          />
+        </div>
+
+        <div className="form-group mb-2">
+          <label htmlFor="flights">
+            Flights Taken (under 4hr) in last year
+          </label>
+          <input
+            type="number"
+            className="form-control"
+            id="flights"
+            placeholder="Flights Taken"
+            required
+            onChange={(e) => setFlights(e.target.value)}
+          />
+        </div>
+
+        <div className="form-group mb-2">
+          <label htmlFor="flights4">
+            Flights Taken (Over 4hr) in last year{" "}
+          </label>
+          <input
+            type="number"
+            className="form-control"
+            id="flights4"
+            placeholder="Flights Taken"
+            required
+            onChange={(e) => setFlightsOver4hr(e.target.value)}
           />
         </div>
 
@@ -31,6 +99,7 @@ function Form() {
           Submit
         </button>
       </form>
+      {footprint}
     </div>
   );
 }
--- src/components/FormContainer.jsx
+++ src/components/FormContainer.jsx
@@ -1,15 +1,19 @@
 import Form from "./Form";
+import { useState } from "react";
 
 function FormContainer() {
+  const [footprint, setFootprint] = useState("");
+  const [imgLink, setImgLink] = useState(
+    "https://slack-imgs.com/?c=1&o1=ro&url=https%3A%2F%2Fassets.justenergy.com%2Fwp-content%2Fuploads%2F2013%2F04%2FiStock_000014351447Medium-1.jpg"
+  );
   return (
-    <div className="form-container">
-      FormContainer
-      <img
-        src="https://cdn.cundall.com/uploads/images/Blog/_large_image/101591/Reducing-our-personal-carbon-footprint.webp?v=1669306425"
-        alt="img"
-        width={400}
+    <div className="form-container p-5">
+      <img src={imgLink} alt="img" width={400} />
+      <Form
+        footprint={footprint}
+        setFootprint={setFootprint}
+        setImgLink={setImgLink}
       />
-      <Form />
     </div>
   );
 }
